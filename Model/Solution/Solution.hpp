5e461095fe238d5f1400d51d0ef294b4d108d275
ElMegos <Mikail.ymz@hotmail.com>
2023-02-12 11:52:36 +0100

    Réarangement fonction template

---
+++ Model/Solution/Solution.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include "Solution.hpp"
+
+using namespace std;
+
+
+
+
+
+
+
+//faire une fonction template generate(générateur, évaluateur, séléectionneur, opérateur de croisement , opérateur de croisement, opérateur de mutation , critère d'arrêt , taille de population)
+// Return type T
