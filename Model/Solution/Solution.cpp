7edcc6c2fe547bff9b1f6bcdf23f3de30f3e0447
ElMegos <Mikail.ymz@hotmail.com>
2023-02-12 23:36:55 +0100

    implémentation fct sur Solution.cpp

--- Model/Solution/Solution.cpp
+++ Model/Solution/Solution.cpp
@@ -1,1 +1,39 @@
-// Fonction et Classe Template donc tout dans le .hpp
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include "Solution.hpp"
+#include "../Ville/Ville.hpp"
+
+
+using namespace std;
+
+Solution::Solution() {
+    taille = 0;
+}
+
+Solution::~Solution() {
+    if(listeVille.size() > 0){
+        for(auto Ville : listeVille){
+            delete Ville;
+        }
+    }
+}
+
+vector<Ville*> Solution::ajoutVille(Ville *ville) {
+    listeVille.push_back(ville);
+    this->taille++;
+}
+
+Ville* Solution::getVille(const int &i) {
+    return listeVille[i];
+}
+
+int Solution::getValeur() {
+    return valeur;
+}
+
+void Solution::setValeur(const double &val) {
+    this->valeur = val;
+}
