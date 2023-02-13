ced9590b99d45bb1f59056afa3d2d25f5c89abca
ElMegos <Mikail.ymz@hotmail.com>
2023-02-14 00:23:43 +0100

    Ajout Evaluateur avec note comme calcul de la distance entre les 2 villes

--- Model/Evaluateur/Evaluateur.cpp
+++ Model/Evaluateur/Evaluateur.cpp
@@ -1,6 +1,29 @@
 #include "Evaluateur.hpp"
 #include "Solution.hpp"
 
+
+//Return la distance entre les villes obtenu comme note
 Evaluateur Evaluateur::getEvaluation(Solution a) {
-    //Return la distance entre les villes obtenu comme note
+
 }
+
+
+
+
+
+/*
+ *
+ * Calculate distance (in km) between two points specified by latitude/longitude with Haversine formula
+LatLong.distHaversine = function(p1, p2) {
+    var R = 6371; // earth's mean radius in km
+    var dLat  = p2.lat - p1.lat;
+    var dLong = p2.lon - p1.lon;
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(p1.lat) * Math.cos(p2.lat) * Math.sin(dLong/2) * Math.sin(dLong/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+
+    return d;
+}
+ */
--- Model/Evaluateur/Evaluateur.hpp
+++ Model/Evaluateur/Evaluateur.hpp
@@ -1,4 +1,18 @@
 //La note va être la distance total entre les points.
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <random>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include "Solution.hpp"
+#include "../Solution/Solution.hpp"
+#include "../Ville/Ville.hpp"
+
+
+using namespace std;
 #include "Solution.hpp"
 
 class Evaluateur {
